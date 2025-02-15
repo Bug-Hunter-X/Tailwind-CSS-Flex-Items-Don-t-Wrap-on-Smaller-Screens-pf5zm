19847f176809b828c19d646ca691441702d1d898
BugHunter-X <askzenai@gmail.com>
2025-02-15 10:14:06 +0100

    Add solution.jsx file

---
+++ solution.jsx
@@ -0,0 +1,12 @@
+```javascript
+// solution.jsx
+<div className="flex flex-wrap">
+  <div className="bg-red-500 p-4">
+    This is a red div
+  </div>
+  <div className="bg-blue-500 p-4">
+    This is a blue div
+  </div>
+</div>
+```
+By adding the `flex-wrap` class, the flex items will wrap onto multiple lines when there is not enough horizontal space, ensuring proper responsive behavior.
